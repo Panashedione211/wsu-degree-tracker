1442c3f1270df275c890a5b631a0fa75a6ff5d61
Panashedione211 <PanasheDione211@gmail.com>
2026-08-06 22:46:00 -0700

    looped through data and added coures, prereqs, and credits to a map

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import json
 
-
+# loads data from a json file 
 def load_json_file(file):
     with open(file, 'r') as file:
         data = json.load(file)
         print("completed")
         return data
     
-    """
+    # fills a map with data from json file
 def fill_map(data):
     map_data = {}
-    for courses in data:
-        map_data[course]
-    """
+    for items in data:
+        course = items['course']
+        prereqs = items['prereqs']
+        credits = items['credits']
+        map_data[course] = {'prerequisites': prereqs, 'credits': credits}
+    return map_data
 
-print(load_json_file('data/courses.json'))
+#print(load_json_file('data/courses.json'))
+
+json_data = load_json_file('data/courses.json')
+print(fill_map(json_data))
