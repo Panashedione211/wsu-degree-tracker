039d5fde1b1ea8342300efbbaf9933cb763d8fc7
Panashedione211 <PanasheDione211@gmail.com>
2026-08-17 00:44:53 -0700

    changed function name to be more accurate. and added more commits to different pieces of code

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def fill_map(data):
         map_data[course] = {'prerequisites': prereqs, 'credits': credits}
     return map_data
 
- # recursicly prints the prerequisites for a given course 
-def print_course_prerequisites(course_map):
+ # prints all the courses in the map and a user inputs a course
+def select_course(course_map):
     for course in course_map:
         print(f"Course: {course}")
     chosen_course = input("Enter a course to see its prerequisites: ")
@@ -47,17 +47,18 @@ def get_whats_left(course_map):
     while user_input := input("Enter your completed courses (or 'done' to finish): ").strip():
         if user_input.lower() == 'done':
             break
+        # add the course and its prerequisites to completed
         elif user_input in course_map:
             completed_courses.add(user_input)
             completed_courses.update(get_full_chain(user_input, course_map))
         else:
             print(f"{user_input} is not a valid course.")
-    
+    # user enters the course they're working towards so it can get compared to completed courses
     chosen_course = input("what course are you working towards? ")
     if chosen_course not in course_map:
         print(f"{chosen_course} is not a valid course.")
         return []
-        
+        # get the needed courses by comparing the full chain of the course they're working towards to the full chain of completed courses and return the difference
     still_needed_courses = set(get_full_chain(chosen_course, course_map)) - completed_courses
     still_needed_courses = sorted(still_needed_courses)  # sort the list alphabetically
     return still_needed_courses
@@ -70,7 +71,7 @@ def get_whats_left(course_map):
 
 json_data = load_json_file('data/courses.json')
 #print(fill_map(json_data))
-#chosen_course = print_course_prerequisites(fill_map(json_data))
+#chosen_course = select_course(fill_map(json_data))
 #print(get_full_chain(chosen_course, fill_map(json_data)))
 print(get_whats_left(fill_map(json_data)))
 
